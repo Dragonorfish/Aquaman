0db3bcf515a1413533d5c9110f5d3e987b538ff2
caspar <2677423874@qq.com>
2023-05-19 17:30:00 +0800

    说说部分50%

--- src/views/articleview/ArticleView.vue
+++ src/views/articleview/ArticleView.vue
@@ -1,23 +1,23 @@
 <template>
-  <div class="page_body">
-    <div class="article_view_body" v-if="!isLoading">
-      <div class="article_box" >
-        <div class="title" id="article_title">{{articleInfo.artTitle}}</div>
-        <div class="article_card" ref="articleCard" v-html="articleInfo.artContent">
+    <div class="page_body">
+        <div class="article_view_body" v-if="!isLoading">
+            <div class="article_box">
+                <div class="title" id="article_title">{{articleInfo.artTitle}}</div>
+                <div class="article_card" ref="articleCard" v-html="articleInfo.artContent">
+                </div>
+                <CommentArea :familyId="articleInfo.id" style="width: 100%;margin-top: 4rem"></CommentArea>
+            </div>
+            <div class="article_info">
+                <UserInfoItem :author="author"></UserInfoItem>
+            </div>
         </div>
-        <CommentArea :familyId="articleInfo.id" style="width: 100%;margin-top: 4rem"></CommentArea>
-      </div>
-      <div class="article_info">
-        <UserInfoItem :author="author"></UserInfoItem>
-      </div>
+        <Loading v-if="isLoading"></Loading>
     </div>
-    <Loading v-if="isLoading"></Loading>
-  </div>
 </template>
 
 
 <style scoped>
-@import "article.css";
+    @import "article.css";
 
 </style>
 
@@ -25,32 +25,32 @@
   import { markdownToHtml, markdownToHtmlWithoutLink } from "../../utils/MarkdownUtils";
   import { doActionByAqBack } from "../../utils/ajaxService";
   import { getServer } from "../../environment/environment";
-  import { useRoute } from 'vue-router';
-  import {ref,reactive,nextTick,onMounted} from "vue"
-  import CommentArea from "../../components/CommentArea.vue"
+  import { useRoute } from "vue-router";
+  import { ref, reactive, nextTick, onMounted } from "vue";
+  import CommentArea from "../../components/CommentArea.vue";
   import router from "../../router";
-  import UserInfoItem from "../../components/UserInfoItem.vue"
+  import UserInfoItem from "../../components/UserInfoItem.vue";
 
 
   const props = defineProps({
-    id:String
-  })
-  const route = useRoute()
-  let isLoading=ref(true)
-  let articleInfo=reactive({
-    id:route.params.id,
-    userId:"",
-    artTitle:"",
-    artContent:"",
-  })
-  let author=ref({})
+    id: String
+  });
+  const route = useRoute();
+  let isLoading = ref(true);
+  let articleInfo = reactive({
+    id: route.params.id,
+    userId: "",
+    artTitle: "",
+    artContent: ""
+  });
+  let author = ref({});
 
-  console.log(articleInfo.userId)
+  console.log(articleInfo.userId);
 
-  onMounted(()=>{
+  onMounted(() => {
     initArticle();
 
-  })
+  });
 
 
   async function initArticle() {
@@ -59,14 +59,12 @@
     };
     const article = ref(await getArticle(queryAriticleData));
     console.log(article.value, articleInfo);
-    articleInfo.artContent = markdownToHtml(article.value.artContent.replace(/^(\s|")+|(\s|")+$/g, '')
-      .replace(/\\n/g, '\n').replace(/\\t/g, '\t').replace(/\\"/g, "\""));
+    articleInfo.artContent = markdownToHtml(article.value.artContent.replace(/^(\s|")+|(\s|")+$/g, "")
+      .replace(/\\n/g, "\n").replace(/\\t/g, "\t").replace(/\\"/g, "\""));
     articleInfo.userId = article.value.userId;
     articleInfo.artTitle = article.value.artTitle;
-    getAuthor({userId:articleInfo.userId});
+    getAuthor({ userId: articleInfo.userId });
   }
-
-
 
 
   function getArticle(queryData) {
@@ -77,9 +75,9 @@
         "getArticleById",
         queryData
       ).subscribe((response) => {
-        resolve(response.data)
+        resolve(response.data);
       });
-    })
+    });
   }
 
   function getAuthor(queryData) {
@@ -89,13 +87,13 @@
       "getAuthor",
       queryData
     ).subscribe((response) => {
-      author.value=response.data;
-      isLoading.value=!isLoading.value
-      nextTick(()=>{
+      author.value = response.data;
+      isLoading.value = !isLoading.value;
+      nextTick(() => {
         document.getElementById("article_title").scrollIntoView({
-          behavior:"smooth"
-        })
-      })
+          behavior: "smooth"
+        });
+      });
     });
   }
 </script>
--- src/views/talkview/TalkView.vue
+++ src/views/talkview/TalkView.vue
@@ -1,13 +1,165 @@
 <template>
-    <div class="talk_body">
+    <div id="talk_body" class="page_body">
+        <h1 class="page_title">说说</h1>
+        <Loading v-if="isLoading"></Loading>
+        <div class="talk_main">
+            <div class="talk_list">
+                <div class="talk_item" v-for="(item,index) in talkList">
+                    <div class="talk_info">
+                        <img :src="item.userAvatar" class="avatar"/>
+                        <span id="user_name" class="info_text">{{item.userName}}</span>
+                        <span id="pub_time" class="info_text">发布于 {{item.addTime}}</span>
+                    </div>
+                    <div class="talk_content">
+                        <div class="talk_text">{{item.talkContent}}</div>
+                        <div class="talk_imgs">
+                            <div :class="item.talkPicList.length===1?
+                                 'one_img':item.talkPicList.length===2?
+                                 'two_img':'more_img'"
+                            >
+                                <div class="img_box" v-for="img in item.talkPicList">
+                                    <img class="img_item"  :src="img"/>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div id="side_part" class="color_box">
+
+            </div>
+        </div>
+
         <Pagination class="pagination" :total="pageNum" @pageChange="pageChange"></Pagination>
     </div>
 </template>
 
 <style scoped>
-    .talk_body {
+    #talk_body {
+        width: 100%;
+        min-height: 500px;
+        display: flex;
+        flex-direction: column;
+        border-radius: 15px;
+        padding: 10px;
+        box-sizing: border-box;
+    }
+    .talk_main{
+        display: flex;
+        flex-direction: row;
+        flex: auto;
+        backdrop-filter:blur(15px);
+    }
+    .talk_list{
+        display: flex;
+        flex-direction: column;
+        flex: 4;
+        margin-right: 4rem;
+        margin-top: 1rem;
+    }
+    #side_part{
+        width: 300px;
+        height: 400px;
+        background-image: linear-gradient(to right bottom , orange,#00a4a2);
+    }
+    .talk_item{
+        width: 100%;
+        min-height: 80px;
+        background-color: #2e2e2e;
+        display: flex;
+        flex-direction: column;
+        padding: 1rem;
+        box-sizing: border-box;
+        border-radius: 25px;
+        margin-bottom: 1rem;
+        transition: transform 0.3s;
+    }
+    .talk_item:hover{
+        transform: scale(1.005);
+    }
+    .avatar{
+        width: 2.5rem;
+        height: 2.5rem;
+        border-radius: 50%;
+        grid-column-start: 1;
+        grid-column-end: 2;
+        grid-row-start: 1;
+        grid-row-end: 3;
+    }
+    .talk_info{
+        display: grid;
+        grid-template-columns: 2.5rem auto;
+        grid-template-rows: 1.25rem 1.25rem;
         width: 100%;
     }
+    .info_text{
+        margin-left: 1rem;
+    }
+    #user_name{
+        font-size: 16px;
+        font-weight: 600;
+        color: whitesmoke;
+        font-style: normal;
+        opacity: 0.8;
+    }
+    #pub_time{
+        font-size: 14px;
+        color: grey;
+        font-style: italic;
+
+    }
+    .talk_content{
+        width: 100%;
+        min-height: 50px;
+        margin-top: 10px;
+        position: relative;
+    }
+    .talk_text{
+        width: 100%;
+        color: whitesmoke;
+        margin-left: 2.5rem;
+    }
+    .talk_imgs{
+        margin-top: 1rem;
+        width: 80%;
+        margin-left: 2.5rem;
+    }
+    .one_img{
+        width: 100%;
+    }
+    .two_img{
+        display: grid;
+        grid-template-columns: repeat(2,49%);
+        grid-gap: 1%;
+    }
+    .more_img{
+        display: grid;
+        grid-template-columns: repeat(3,33%);
+        grid-gap: 0.5%;
+    }
+    .img_box{
+        width: 100%;
+        height: 100%;
+        background-color: #2b2b2b;
+        transition: transform 0.3s;
+    }
+    .img_box:hover{
+        transform: scale(1.02);
+        cursor: url("../../assets/pointer1.cur"),auto;
+    }
+    .img_item{
+        width: 100%;
+        height: 100%;
+        max-height: 40rem;
+        min-height: 10rem;
+        object-fit: contain;
+    }
+    @media (max-width: 1024px) {
+        #side_part{
+            display: none;
+        }
+    }
+
 </style>
 
 <script setup>
@@ -21,6 +173,7 @@
   };
   const pageNum = ref(0);
   const talkList = ref([]);
+  let isLoading = ref(true);
 
   initList(queryData);
 
@@ -33,6 +186,7 @@
     ).subscribe((response) => {
       pageNum.value = response.data.totalPage;
       talkList.value = response.data.content;
+      changeLoadingState()
     });
   }
 
@@ -53,6 +207,11 @@
       //   behavior: "smooth"
       // });
     });
+  }
+
+  function changeLoadingState() {
+    console.log(isLoading.value);
+    isLoading.value = !isLoading.value;
   }
 </script>
 
