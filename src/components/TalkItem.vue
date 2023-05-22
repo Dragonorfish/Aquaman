a87ddc3032454a4442cb58f3fcbc4bdade8538c5
caspar <2677423874@qq.com>
2023-05-22 16:10:55 +0800

    说说fix

--- src/components/TalkItem.vue
+++ src/components/TalkItem.vue
@@ -152,7 +152,7 @@
   const needCover = ref(false);
   const urlreg = /((?:(http|https|Http|Https|rtsp|Rtsp):\/\/(?:(?:[a-zA-Z0-9\$\-\_\.\+\!\*\'\(\)\,\;\?\&\=]|(?:\%[a-fA-F0-9]{2})){1,64}(?:\:(?:[a-zA-Z0-9\$\-\_\.\+\!\*\'\(\)\,\;\?\&\=]|(?:\%[a-fA-F0-9]{2})){1,25})?\@)?)?((?:(?:[a-zA-Z0-9][a-zA-Z0-9\-]{0,64}\.)+(?:(?:aero|arpa|asia|a[cdefgilmnoqrstuwxz])|(?:biz|b[abdefghijmnorstvwyz])|(?:cat|com|coop|c[acdfghiklmnoruvxyz])|d[ejkmoz]|(?:edu|e[cegrstu])|f[ijkmor]|(?:gov|g[abdefghilmnpqrstuwy])|h[kmnrtu]|(?:info|int|i[delmnoqrst])|(?:jobs|j[emop])|k[eghimnrwyz]|l[abcikrstuvy]|(?:mil|mobi|museum|m[acdghklmnopqrstuvwxyz])|(?:name|net|n[acefgilopruz])|(?:org|om)|(?:pro|p[aefghklmnrstwy])|qa|r[eouw]|s[abcdeghijklmnortuvyz]|(?:tel|travel|t[cdfghjklmnoprtvwz])|u[agkmsyz]|v[aceginu]|w[fs]|y[etu]|z[amw]))|(?:(?:25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[1-9])\.(?:25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[1-9]|0)\.(?:25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[1-9]|0)\.(?:25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[0-9])))(?:\:\d{1,5})?)(\/(?:(?:[a-zA-Z0-9\;\/\?\:\@\&\=\#\~\-\.\+\!\*\'\(\)\,\_])|(?:\%[a-fA-F0-9]{2}))*)?(?:\b|$)/gi;
   const content = props.talkInfo.talkContent.replace(urlreg, (url) => {
-    return "<a class=\"text_link\" href=\"" + url + "\">" + url + "</a>";
+    return '<a class="text_link" href="' + url + '">' + url + '</a>';
   });
   onMounted(() => {
     document.getElementById(props.talkInfo.id).getElementsByClassName("talk_text_detail")[0].innerHTML = content;
--- src/views/talkview/TalkView.vue
+++ src/views/talkview/TalkView.vue
@@ -4,29 +4,8 @@
         <Loading v-if="isLoading" style="width:100%;height:100vh"></Loading>
         <div v-if="!isLoading" class="talk_main">
             <div class="talk_list">
-                <div class="talk_item" v-for="(item,index) in talkList" :id="item.id">
-                    <div class="talk_info">
-                        <img :src="item.userAvatar" class="avatar"/>
-                        <span id="user_name" class="info_text">{{item.userName}}</span>
-                        <span id="pub_time" class="info_text">发布于 {{item.addTime}}</span>
-                    </div>
-                    <div class="talk_content">
-                        <div class="talk_text">
-                            {{item.talkContent}}
-                            <div class="talk_text_cover"></div>
-                        </div>
-                        <div class="talk_imgs">
-                            <div :class="item.talkPicList.length===1?
-                                 'one_img':item.talkPicList.length===2?
-                                 'two_img':'more_img'"
-                            >
-                                <div class="img_box" v-for="img in item.talkPicList">
-                                    <AqImage :url="img"/>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                <TalkItem v-for="(item,index) in talkList" :talkInfo="item" :id="item.id">
+                </TalkItem>
             </div>
             <div id="side_part" class="color_box">
 
